be39f2c01736cf0af9e6188f70ee9578ad35ee51
TommyPai <tommyisme@gmail.com>
2018-03-15 09:05:20 +0800

    comit1

---
+++ TEST_GIT/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TEST_GIT.Startup))]
+namespace TEST_GIT
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
